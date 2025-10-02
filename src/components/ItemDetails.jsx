7f4ca5a651747872a7ec398a1795abd669785b92
Sandeepraina40 <127074583+Sandeepraina40@users.noreply.github.com>
2025-10-02 16:30:48 +0530

    completed

--- src/components/ItemDetails.jsx
+++ src/components/ItemDetails.jsx
@@ -1,24 +1,24 @@
-import React from 'react'
-import {useParames} from 'react-router-dom'
+import React from "react";
+import { useParams } from "react-router-dom";
 
-const ItemDetails = () => {
-    const {id} = useParams();
-    const itemData = {
+const ItemDetail = () => {
+  const { id } = useParams(); // v5+ hook
+  const itemData = {
     1: { name: "Item 1", description: "Description for Item 1" },
     2: { name: "Item 2", description: "Description for Item 2" },
     3: { name: "Item 3", description: "Description for Item 3" }
   };
-   const item = itemData[id];
 
-  if (!item) {
-    return <h1>Item not found</h1>;
-  }
+  const item = itemData[id];
+
+  if (!item) return <h1>Item not found</h1>;
+
   return (
     <div>
       <h1>{item.name}</h1>
       <p>{item.description}</p>
     </div>
-  )
-}
+  );
+};
 
-export default ItemDetails
+export default ItemDetail;
